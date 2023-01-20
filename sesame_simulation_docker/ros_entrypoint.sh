2485d48f7ba429d30cbd5386c788a8d7ee3269d9
AntoineRichard <antoine.richard@gatech.edu>
2023-01-20 14:11:31 +0100

    forgot a git...

--- sesame_simulation_docker/ros_entrypoint.sh
+++ sesame_simulation_docker/ros_entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # setup ros environment
-source "/opt/ros/$ROS_DISTRO/setup.bash" --
-source "/opt/sesame_ws/devel/setup.bash" --
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+source "/opt/sesame_ws/devel/setup.bash"
 exec "$@"
